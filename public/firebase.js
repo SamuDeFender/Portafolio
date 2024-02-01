c57135dc99ea11ac73f2837859c6b1b01cd7ff47
Samuel Ortega Olguin <xXSamxYeahhxX@gmail.com>
2024-02-01 16:56:14 -0300

    Agrega nuevas funcionalidades (simples html) para botones de pantalla principal

--- public/firebase.js
+++ public/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-analytics.js";
-//import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-storage.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-storage.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
